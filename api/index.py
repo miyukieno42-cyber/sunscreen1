52570cad13bf5f269226d7cafef5d8a3602f6942
miyukieno42-cyber <miyuki.eno42@gmail.com>
2026-08-23 19:28:30 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,7 +5,15 @@ import io
 import os
 from datetime import datetime
 
-app = Flask(__name__)
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+app = Flask(
+    __name__,
+    template_folder=os.path.join(BASE_DIR, 'templates'),
+    static_folder=os.path.join(BASE_DIR, 'static')
+)
 
 # Vercelの一時保存フォルダ（/tmp）にDBを作成
 DATABASE = "/tmp/survey.db"
